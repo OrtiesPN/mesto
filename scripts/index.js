df1c7222bf2e094798ec77e3dfd79c8501c5b4b9
Nikita Petukhov <ortiespn@yandex.ru>
2023-03-10 20:01:46 +0530

    v1 all features

--- scripts/index.js
+++ scripts/index.js
@@ -2,22 +2,32 @@ console.log("Wake up, Neo!");
 const editButtonElement = document.querySelector('.profile__edit-button'); // получаю элемент кнопки редактирования
 const popupElement = document.querySelector('.popup'); // получаю элемент секции попап
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button'); // получаю элемент кнопки закрытия попапа из секции попапа
-
+console.log(editButtonElement, popupElement, popupCloseButtonElement);
 let profileName = document.querySelector('.profile__name'); // получаю элемент имени профиля
 let profileJob = document.querySelector('.profile__subtitle'); // получаю элемент описания профиля
-
+console.log(profileName.textContent, profileJob.textContent);
 const popupEditProfileElement = document.querySelector('.popup__edit-profile'); // получаю элемент формы редактирования профиля
-let nameInput = popupEditProfileElement.querySelector('.popup__name-input'); // получаю элемент строки ввода имени из формы
-let jobInput = popupEditProfileElement.querySelector('.popup__job-input'); // получаю элемент строки ввода описания из формы
-
+let nameInput = popupEditProfileElement.querySelector('#name.popup__input'); // получаю элемент строки ввода имени из формы
+let jobInput = popupEditProfileElement.querySelector('#job.popup__input'); // получаю элемент строки ввода описания из формы
+console.log(popupEditProfileElement, nameInput.value, jobInput.value);
 const openPopup = function () { // функция открытия попапа, каждый вызов функции подставляет в строки ввода исходные значения из профиля
     popupElement.classList.add('popup_is-active');
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
+    console.log(popupElement.classList, nameInput.value, jobInput.value);
+};
+
+const clickNameInput = function () { // 
+    nameInput.value = '';
+};
+
+const clickJobInput = function () {
+    jobInput.value = '';
 };
 
 const closePopup = function () { // функция закрытия попапа по крестику
     popupElement.classList.remove('popup_is-active');
+    console.log(popupElement.classList);
 };
 
 const closePopupByClickOverlay = function (event) { // функция закрытия попапа по черному полю с ранним выходом
@@ -25,6 +35,7 @@ const closePopupByClickOverlay = function (event) { // функция закры
         return;
     }
     closePopup();
+    console.log(popupElement.classList);
 };
 
 function handleFormSubmit (evt) {
@@ -32,16 +43,14 @@ function handleFormSubmit (evt) {
     profileName.textContent = nameInput.value; // подставляю в профиль значения из строк ввода
     profileJob.textContent = jobInput.value;
     closePopup(); // закрываю попап
-
-
-    // Получите значение полей jobInput и nameInput из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
+    console.log(nameInput.value, profileName.textContent, jobInput.value, profileJob.textContent);
 }
 
+// Раздел с обработчиками событий
+
 editButtonElement.addEventListener("click", openPopup);
+nameInput.addEventListener("click", clickNameInput);
+jobInput.addEventListener("click", clickJobInput);
 popupCloseButtonElement.addEventListener("click", closePopup);
 popupElement.addEventListener("click", closePopupByClickOverlay);
 popupEditProfileElement.addEventListener('submit', handleFormSubmit);
