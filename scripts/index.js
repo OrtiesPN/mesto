4a7eff5afce22ac138ee7c644162a3a719935df4
Nikita Petukhov <ortiespn@yandex.ru>
2023-03-13 15:31:04 +0530

    hotfixes/1st review done

--- scripts/index.js
+++ scripts/index.js
@@ -1,59 +1,33 @@
-console.log("Wake up, Neo!");
-const editButtonElement = document.querySelector('.profile__edit-button'); // получаю элемент кнопки редактирования
+const ButtonEditProfileElement = document.querySelector('.button__edit'); // получаю элемент кнопки редактирования
 const popupElement = document.querySelector('.popup'); // получаю элемент секции попап
-const popupCloseButtonElement = popupElement.querySelector('.popup__close-button'); // получаю элемент кнопки закрытия попапа из секции попапа
-console.log(editButtonElement, popupElement, popupCloseButtonElement);
-let profileName = document.querySelector('.profile__name'); // получаю элемент имени профиля
-let profileJob = document.querySelector('.profile__subtitle'); // получаю элемент описания профиля
-console.log(profileName.textContent, profileJob.textContent);
+const popupCloseButtonElement = document.querySelector('.button__close-popup'); // получаю элемент кнопки закрытия попапа из секции попапа
+let profileNameElement = document.querySelector('.profile__name'); // получаю элемент имени профиля
+let profileJobElement = document.querySelector('.profile__subtitle'); // получаю элемент описания профиля
 const popupEditProfileElement = document.querySelector('.popup__edit-profile'); // получаю элемент формы редактирования профиля
-let nameInput = popupEditProfileElement.querySelector('#name.popup__input'); // получаю элемент строки ввода имени из формы
-let jobInput = popupEditProfileElement.querySelector('#job.popup__input'); // получаю элемент строки ввода описания из формы
-console.log(popupEditProfileElement, nameInput.value, jobInput.value);
+let nameInputElement = document.getElementById('name'); // получаю элемент строки ввода имени из формы
+let jobInputElement = document.getElementById('job'); // получаю элемент строки ввода описания из формы
 
 const openPopup = function () { // функция открытия попапа, каждый вызов функции подставляет в строки ввода исходные значения из профиля
-    popupElement.classList.add('popup_is-active');
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileJob.textContent;
-    console.log(popupElement.classList, nameInput.value, jobInput.value);
-};
-
-const clickNameInput = function () { // 
-    nameInput.value = '';
-};
-
-const clickJobInput = function () {
-    jobInput.value = '';
+    popupElement.classList.add('popup_opened');
+    nameInputElement.value = profileNameElement.textContent;
+    jobInputElement.value = profileJobElement.textContent;
 };
 
 const closePopup = function () { // функция закрытия попапа по крестику
-    popupElement.classList.remove('popup_is-active');
-    console.log(popupElement.classList);
-};
-
-const closePopupByClickOverlay = function (event) { // функция закрытия попапа по черному полю с ранним выходом
-    if (event.target !== event.currentTarget) {
-        return;
-    }
-    closePopup();
-    console.log(popupElement.classList);
+    popupElement.classList.remove('popup_opened');
 };
 
 function handleFormSubmit (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    profileName.textContent = nameInput.value; // подставляю в профиль значения из строк ввода
-    profileJob.textContent = jobInput.value;
+    profileNameElement.textContent = nameInputElement.value; // подставляю в профиль значения из строк ввода
+    profileJobElement.textContent = jobInputElement.value;
     closePopup(); // закрываю попап
-    console.log(nameInput.value, profileName.textContent, jobInput.value, profileJob.textContent);
 }
 
 // Раздел с обработчиками событий
 
-editButtonElement.addEventListener("click", openPopup);
-nameInput.addEventListener("click", clickNameInput);
-jobInput.addEventListener("click", clickJobInput);
+ButtonEditProfileElement.addEventListener("click", openPopup);
 popupCloseButtonElement.addEventListener("click", closePopup);
-popupElement.addEventListener("click", closePopupByClickOverlay);
 popupEditProfileElement.addEventListener('submit', handleFormSubmit);
 
 
