bd940aa4b753cd9c4262314bcebbecd19be14598
Nikita Petukhov <ortiespn@yandex.ru>
2023-05-29 15:34:13 +0300

    do main functionality

--- index.js
+++ index.js
@@ -10,58 +10,54 @@ import PopupWithForm from './scripts/components/PopupWithForm.js';
 const buttonEditProfileElement = document.querySelector('.profile__edit-btn');
 const buttonAddCardElement = document.querySelector('.profile__add-btn'); 
 
-
-
-const formsList = document.querySelectorAll('form');
-
-const popupEditProfileForm = document.querySelector('#edit-profile-form');
-const nameInputElement = document.querySelector('#name');
-const jobInputElement = document.querySelector('#job'); 
-
+const popupEditProfileForm = document.querySelector('#edit-profile-form'); 
 const popupAddCardForm = document.querySelector('#add-card-form');
-const placeInputElement = document.querySelector('#place');
-const linkInputElement = document.querySelector('#link');
-const submitCardButton = popupAddCardForm.querySelector(validationConfig.submitButtonSelector);
-
-
 
 // Селекторы и классы
+
 const profileNameSelector = ('.profile__name');
 const profileJobSelector = ('.profile__subtitle');
 
 const sectionSelector = ('.elements__cards');
 
 const editProfilePopupSelector = ('.edit-profile-popup');
-const addCardPopup = ('.add-card-popup');
+const addCardPopupSelector = ('.add-card-popup');
 const imageCardPopupSelector = ('.image-card-popup');
 
 const cardTemplate = ('.card-template');
 
 // Обработчики Popup
+
 const userInfo = new UserInfo (profileNameSelector, profileJobSelector);
 
-const editProfilePopup = new PopupWithForm (editProfilePopupSelector,
-  handleEditProfileFormSubmit);
+const editProfilePopup = new PopupWithForm (editProfilePopupSelector, handleEditProfileFormSubmit);
 editProfilePopup.setEventListeners();
 
-const addCardPopup = new PopupWithForm ()
+const addCardPopup = new PopupWithForm (addCardPopupSelector, handleAddCardFormSubmit);
+addCardPopup.setEventListeners();
 
 const imageCardPopup = new PopupWithImage (imageCardPopupSelector)
 imageCardPopup.setEventListeners();
 
-const cardSection = new Section ( {items: initialCards, 
-  renderer: (element) => {
-    return new Card(element, cardTemplate, imageCardPopup.open).createNewCard();
-  }}, sectionSelector);
+const cardSection = new Section ( { items: initialCards, 
+  renderer: addCardRenderer }, sectionSelector);
 cardSection.addInitialItems ();
 
-
-
-
 // Функции callback
+
 function toggleEditProfilePopup() {
   editProfilePopup.setInputValues(userInfo.getUserInfo());
   editProfilePopup.open();
+}
+
+function toggleAddCardPopup() {
+  validationPopupAddCardForm.setButtonStatus();
+  validationPopupAddCardForm.clearErrors();
+  addCardPopup.open();
+}
+
+function addCardRenderer(element) {
+  return new Card(element, cardTemplate, imageCardPopup.open).createNewCard();
 }
 
 function handleEditProfileFormSubmit(formValues) {
@@ -69,30 +65,20 @@ function handleEditProfileFormSubmit(formValues) {
   editProfilePopup.close(); 
 }
 
-const handleAddCardFormSubmit = (evt) => {
-  evt.preventDefault();
-  const userCard = {name: placeInputElement.value, link: linkInputElement.value};
-  prependCard(userCard);
-  // closePopup(addCardPopup); 
-  popupAddCardForm.reset();
+function handleAddCardFormSubmit(formValues) {
+  cardSection.prependItem(formValues);
+  addCardPopup.close();
 }
 // Функции обработки форм
-
-formsList.forEach((form) => {
-  form = new FormValidator(validationConfig, form);
-  form.enableValidation();
-})
 
 const validationEditProfileForm = new FormValidator(validationConfig, popupEditProfileForm);
 validationEditProfileForm.enableValidation();
 
-const validationpopupAddCardForm = new FormValidator(validationConfig, popupAddCardForm);
-validationpopupAddCardForm.enableValidation();
+const validationPopupAddCardForm = new FormValidator(validationConfig, popupAddCardForm);
+validationPopupAddCardForm.enableValidation();
 
 // Раздел с обработчиками событий
 
-
 buttonEditProfileElement.addEventListener('click', toggleEditProfilePopup);
-// buttonAddCardElement.addEventListener('click', openAddCardPopup);
-// popupAddCardForm.addEventListener('submit', handleAddCardFormSubmit);
+buttonAddCardElement.addEventListener('click', toggleAddCardPopup);
 
